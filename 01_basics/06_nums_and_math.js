8f4713f5e5c046b12d162e3652c757973afa673c
tinabenita <tina1717rego@gmail.com>
2025-03-20 16:42:38 +0530

    task: added notes for nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,7 +1,7 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100) //explicitly creating a number object
 // console.log(balance);
 
 // console.log(balance.toString().length);
@@ -17,7 +17,7 @@ const hundreds = 1000000
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
+// console.log(Math.abs(-4)); // sign change, negative to positive 
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
