810b3cccfc9cd96a850111501d4e5e29fcacdce9
tinabenita <tina1717rego@gmail.com>
2025-03-21 15:00:11 +0530

    task: solutions for arrays: push, pop, shift, unshift, slice, splice

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,36 +4,36 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+console.log(myArr[1]); // 1 
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.push(6) // [0, 1, 2, 3, 4, 5, 6]
+myArr.push(7) // [0, 1, 2, 3, 4, 5, 6, 7]
+myArr.pop() // [0, 1, 2, 3, 4, 5, 6]
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.unshift(9) // [9, 0, 1, 2, 3, 4, 5, 6]
+myArr.shift() // [0, 1, 2, 3, 4, 5, 6]
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log( newArr);
+console.log(myArr);
+console.log( newArr);
 
 
 // slice, splice
 
-console.log("A ", myArr);
+console.log("A ", myArr); 
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3) // indexes 1,2
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) // hides 1,2,3
 console.log("C ", myArr);
 console.log(myn2);
