f222f33b15a8cac6052585e886e45b1216009bcb
tinabenita <tina1717rego@gmail.com>
2025-03-24 14:05:50 +0530

    task: immediately invoked function expressions

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -10,3 +10,6 @@
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
 
+// This means that the variables declared inside the IIFE are not accessible from outside the IIFE. In detail it is used to avoid naming conflicts.
+// IIFE is used to avoid global scope pollution.
+
