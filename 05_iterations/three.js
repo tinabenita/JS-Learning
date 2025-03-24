3e6d7fa22c9a0a507077b193c6d2a4783d4514c8
tinabenita <tina1717rego@gmail.com>
2025-03-24 15:05:03 +0530

    task: solutions for for-of, map vs object

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -6,27 +6,27 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    //console.log(num);
+    //console.log(num); // 1, 2, 3, 4, 5
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    //console.log(`Each char is ${greet}`) // H, e, l, l, o, , w, o, r, l, d, !
 }
 
 // Maps
 
-const map = new Map()
+const map = new Map() // map is special in js because it can store any type of data as key and no duplicate keys are allowed. Duplicate values are allowed.
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
 map.set('IN', "India")
 
 
-// console.log(map);
+// console.log(map); // Map(3) { 'IN' => 'India', 'USA' => 'United States of America', 'Fr' => 'France' }
 
 for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+    // console.log(key, ':-', value); // IN :- India, USA :- United States of America, Fr :- France
 }
 
 const myObject = {
@@ -35,6 +35,6 @@ const myObject = {
 }
 
 // for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
+//     console.log(key, ':-', value); // TypeError: myObject is not iterable
     
 // }
