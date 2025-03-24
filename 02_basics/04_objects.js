abf92c1a70a4a5cc21610718fecaaacce1efc76c
tinabenita <tina1717rego@gmail.com>
2025-03-24 10:49:33 +0530

    task: nested objects, combining objects, array of objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,11 +1,11 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() // singleton object
+const tinderUser = {} // non singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+// console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 const regularUser = {
     email: "some@gmail.com",
@@ -17,42 +17,42 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname); // hitesh
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 } // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+//  const obj3 = Object.assign({}, obj1, obj2, obj4) // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+const obj3 = {...obj1, ...obj2} // spread operator
+// console.log(obj3);  // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 
 const users = [
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "h1@gmail.com"
     },
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "h2@gmail.com"
     },
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "h3@gmail.com"
     },
-]
+] // array of objects usually comes from backend
 
-users[1].email
+console.log(users[1].email); // 1 is the index of the array
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); // shows key value pairs
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 const course = {
