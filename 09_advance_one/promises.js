6399a4c8b6bc6ee68d9c9734e5c851bd4a9bcbad
tinabenita <tina1717rego@gmail.com>
2025-03-25 12:31:07 +0530

    task: promise, resolve, then, chaining, catch, finally, async, await

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,14 +1,31 @@
+// A Promise in JavaScript is an object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value. It allows you to write asynchronous code in a more synchronous and manageable way, avoiding the so-called "callback hell."
+
+
+/*
+
+States:
+Pending: The initial state, neither fulfilled nor rejected.
+Fulfilled: The operation completed successfully, and the promise has a resulting value.
+Rejected: The operation failed, and the promise has a reason for the failure (an error).
+
+Methods:
+then(): Attaches callbacks for the resolution (fulfillment) of the promise. It takes two arguments: a callback for when the promise is fulfilled and another for when it is rejected (the second argument is optional).
+catch(): Attaches a callback for the rejection of the promise. It is a shorthand for then(null, rejectionCallback).
+finally(): Attaches a callback that is invoked when the promise is settled (either fulfilled or rejected). It is useful for cleanup operations.
+
+*/
+
 const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
     // DB calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is compelete');
-        resolve()
+        resolve() // Used to connect to .then because it is resolved
     }, 1000)
 })
 
 promiseOne.then(function(){
-    console.log("Promise consumed");
+    console.log("Promise consumed"); // .then will only be called when the promise is resolved
 })
 
 new Promise(function(resolve, reject){
@@ -46,11 +63,11 @@ const promiseFour = new Promise(function(resolve, reject){
  .then((user) => {
     console.log(user);
     return user.username
-}).then((username) => {
+}).then((username) => { // chaining happens here. the value from the previous then gets sent to the next then
     console.log(username);
 }).catch(function(error){
     console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
+}).finally(() => console.log("The promise is either resolved or rejected")) // finally is called no matter what
 
 
 
