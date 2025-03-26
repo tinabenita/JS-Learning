9d0221089c4a7baa265a4836b0a3088a466765a5
tinabenita <tina1717rego@gmail.com>
2025-03-26 12:17:16 +0530

    task: static properties in classes

--- 10_classes_and_oop/staticprop.js
+++ 10_classes_and_oop/staticprop.js
@@ -7,13 +7,13 @@ class User {
         console.log(`Username: ${this.username}`);
     }
 
-    static createId(){
+    static createId(){ // prevents access to instantiated objects
         return `123`
     }
 }
 
 const hitesh = new User("hitesh")
-// console.log(hitesh.createId())
+// console.log(hitesh.createId()) // error as createId is static so can't be accessed by object
 
 class Teacher extends User {
     constructor(username, email){
@@ -23,4 +23,4 @@ class Teacher extends User {
 }
 
 const iphone = new Teacher("iphone", "i@phone.com")
-console.log(iphone.createId());
+console.log(iphone.createId()); // error as createId is static so can't be accessed by object
